9e04d1ba326464e6a3d3c938de43c71166e72242
unknown <ovcharenko.olga.2014@gmail.com>
2024-02-21 23:03:05 +0300

    create endpoint for update product data

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Param, Delete, Post, Body } from '@nestjs/common';
+import { Controller, Get, Param, Delete, Post, Body, Put } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDTO } from './dtos/create-product.dto';
 import { ParseUUIDPipe } from '@nestjs/common';
+import { UpdateProductDTO } from './dtos/update-product.dto';
 
 @Controller('products')
+
 export class ProductsController {
     constructor(private productsService: ProductsService) {}
 
@@ -27,5 +29,13 @@ export class ProductsController {
     @Post('/')
        create(@Body() productData: CreateProductDTO) {
         return this.productsService.create(productData);
+    }
+
+    @Put('/products/:id')
+    update(
+        @Param('id', new ParseUUIDPipe()) id: string,
+        @Body() productData: UpdateProductDTO,
+      ){
+        return this.productsService.updateById(id, productData);
     }
 }
