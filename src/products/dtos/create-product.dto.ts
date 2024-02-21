20f62f9b5167ae8fae7b1aa4056767b9b67808d5
unknown <ovcharenko.olga.2014@gmail.com>
2024-02-21 22:49:06 +0300

    change dto by adding class validation

--- src/products/dtos/create-product.dto.ts
+++ src/products/dtos/create-product.dto.ts
@@ -1,5 +1,23 @@
-export type CreateProductDTO = {
-    name: string;
-    price: number;
-    description: string;
-};
+import { Transform } from 'class-transformer';
+import {
+  IsInt,
+  IsNotEmpty,
+  IsString,
+  Length,
+  Min,
+} from 'class-validator';
+
+export class CreateProductDTO {
+  @IsNotEmpty()
+  @IsString()
+  @Length(10, 20)
+  name: string;
+
+  @IsNotEmpty()
+  @IsInt()
+  @Min(0)
+  price: number;
+
+  @Transform(({ value }) => (Array.isArray(value) ? value.join(', ') : ''))
+  description: string;
+}
